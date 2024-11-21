5b92902d9d92e8a41600ce2c3c204867735974df
Debabrata Mondal <debabrata.js@protonmail.com>
2024-11-22 02:30:03 +0530

    test new controller

--- sdn/controller.py
+++ sdn/controller.py
@@ -11,13 +11,34 @@ class Controller(app_manager.RyuApp):
     def __init__(self, *_args, **_kwargs):
         super(Controller, self).__init__(*_args, **_kwargs)
 
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)    # type: ignore
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
-        ip_pkt = pkt.get_protocol(ipv4.ipv4)
 
-        if ip_pkt:
-            self.logger.info(f"IP packet fro {ip_pkt.src} to {ip_pkt.dst}") # type: ignore
+        # Ignore LLDP packets
+        if eth.ethertype == 0x88cc:
+            return
+
+        ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ipv4_pkt:
+            self.logger.info("Source IP: %s, Destination IP: %s",
+                             ipv4_pkt.src, ipv4_pkt.dst)
+
+        # Install a flow to reduce packet-in events
+        match = parser.OFPMatch(eth_type=0x0800, ipv4_src=ipv4_pkt.src, ipv4_dst=ipv4_pkt.dst)
+        actions = [parser.OFPActionOutput(ofproto.OFPP_NORMAL)]
+        self.add_flow(datapath, 1, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+        datapath.send_msg(mod)
